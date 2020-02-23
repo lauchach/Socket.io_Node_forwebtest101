ad40bb124286f9205451fb2f56dc72d236f9af81
lauchach <tarnapot.b38@gmail.com>
2020-02-23 07:17:11 +0700

    07:17

--- server.js
+++ server.js
@@ -23,19 +23,26 @@ ChatModel.find((err, result) => {
 
 	messages = result;
 });
-io.on("connect", socket => {
-	socket.emit('loggedIn', {
-		users: users.map(s => s.username),
-		messages: messages
-	});
+io.on('connection', socket => {
+	console.log('IO Connected')
+	// socket.emit('loggedIn', {
+	// 	users: users.map(s => s.username),
+	// 	messages: messages
+	// });
 
 	socket.on('newuser', username => {
-		console.log(`${username} has arrived at the party.`);
+		console.log('1111')
+		// console.log(`${username} has arrived at the party.`);
 		socket.username = username;
 		
 		users.push(socket);
 
-		io.emit('userOnline', socket.username);
+		// socket.broadcast.emit('userOnline', socket.username);
+		socket.emit('loggedIn', {
+			users: users.map(s => s.username),
+			messages: messages
+		});
+		socket.broadcast.emit('userOnline', socket.username);
 	});
 
 	socket.on('msg', msg => {
@@ -65,7 +72,7 @@ io.on("connect", socket => {
 		socket.broadcast.emit("userLeft", socket.username);
 		users.splice(users.indexOf(socket), 1);
 	});
-});
+}); 	
 
 http.listen(process.env.PORT || 3000, () => {
 	console.log("Listening on port %s", process.env.PORT || 3000);
